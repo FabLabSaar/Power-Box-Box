24070d836ead23026c46e2bec6ddd88be32a2c19
Florian Fromm <flrn.frmm@gmail.com>
2019-10-18 07:45:05 +0200

    Update

--- PBB-Frame.py
+++ PBB-Frame.py
@@ -1,26 +1,36 @@
 from solid import *
 from solid.utils import *
 
-def bohrungen(wall_thickness):
+wall_thickness = 3
+y_offset = 12 - 3 * wall_thickness
+inner_size = [110, 200 + y_offset - 2 * wall_thickness, 45]
+inner_cube = cube(size=inner_size)
+outer_size = [inner_size[0] + 2 * wall_thickness, inner_size[1] + 0.1 + 1 * wall_thickness, inner_size[2]]
+outer_cube = cube(size=outer_size)
+
+def bohrungen():
     bohrung = union()(
        translate([0,0,2.2])(cylinder(h=2 * wall_thickness, d=3.5, segments=100)),
         cylinder(r2=1.75, r1=3.3, h=2.3, segments=100),)
     bohrungen = []
     for x,y in [(x,y) for x in [0,80] for y in [0, 120]]:
         bohrungen.append(translate([x,y,0])(bohrung))
-    return translate([10 + wall_thickness, 61 + wall_thickness, -0.01])(union()(bohrungen))
+    return translate([15 + wall_thickness, 61 + wall_thickness, -0.01])(union()(bohrungen))
 
-wall_thickness = 3
-y_offset = 12
-inner_size = [100, 200 + y_offset, 42]
-inner_cube = cube(size=inner_size)
-outer_size = list(map(lambda x: x + 2 * wall_thickness, inner_size))
-outer_cube = cube(size=outer_size)
-
-frame = difference()(
+box = difference()(
     outer_cube,
-    translate([wall_thickness, wall_thickness, 2 * wall_thickness + wall_thickness * 0.01])(inner_cube),
-    translate([0, y_offset, 0])(bohrungen(wall_thickness))
-)
+    translate([wall_thickness, -0.1, 2 * wall_thickness + 0.1])(inner_cube),
+    translate([0, -y_offset, 0])(bohrungen()))
 
-scad_render_to_file(frame, 'PBB.scad')
+clutch = difference()(
+    cube([outer_size[0], 3 * wall_thickness, outer_size[2]]),
+    translate([2 * wall_thickness, -0.1, 2 * wall_thickness])(
+        cube([outer_size[0] - 4 * wall_thickness, 3 * wall_thickness + 0.2, outer_size[2]])),
+    translate([wall_thickness, wall_thickness, -0.1])(
+        cube([outer_size[0] - 2 * wall_thickness, wall_thickness, outer_size[2] + 0.2])))
+
+frame = union()(
+    translate([0, 3 * wall_thickness - 0.1, 0])(box),
+    clutch)
+
+scad_render_to_file(frame, 'PBB-Frame.scad')
--- PBB-Front.py
+++ PBB-Front.py
@@ -1,8 +1,53 @@
 from solid import *
+from solid.utils import *
 
+PBB_size = [100, 200, 42]
+wall_thickness = 3
+front_size = [PBB_size[2] - wall_thickness, PBB_size[0] - 2 * wall_thickness, 3]
+pyramid_height = 20
+pyramid_top_offset = 2 * wall_thickness
+pyramid_bottom_offset = 2 * wall_thickness
+usb_size = [13.5, 15, 3 * pyramid_height]
 
-front = union()(
-    cube([10,10,2])    
-)
+def pyramid():
+    points = [
+        [ 0,  0,  0 ],
+        [ front_size[0] - pyramid_bottom_offset,  0,  0 ],
+        [ front_size[0] - pyramid_bottom_offset,  front_size[1] - pyramid_bottom_offset,  0 ],
+        [  0,  front_size[1] - pyramid_bottom_offset,  0 ],
+        [  0 + pyramid_top_offset,  0 + pyramid_top_offset,  pyramid_height],
+        [ front_size[0] - pyramid_top_offset - pyramid_bottom_offset,  0 + pyramid_top_offset,  pyramid_height],
+        [ front_size[0] - pyramid_top_offset - pyramid_bottom_offset,  front_size[1] - pyramid_top_offset - pyramid_bottom_offset,  pyramid_height],
+        [  0 + pyramid_top_offset, front_size[1] - pyramid_top_offset - pyramid_bottom_offset,  pyramid_height]]
+    faces = [
+        [0,1,2,3],
+        [4,5,1,0],
+        [7,6,5,4],
+        [5,6,2,1],
+        [6,7,3,2],
+        [7,4,0,3]]
+    return polyhedron(points, faces)
+
+def usb():
+    outer_size = [usb_size[0] * 1.1, usb_size[1] * 1.1, 1] 
+    return (union()(
+        up(pyramid_height + front_size[2])(cube(outer_size, center=True)),
+        cube(usb_size, center=True)))
+
+def usbs():
+    return union()([translate([0, (usb_size[0] + ((front_size[1] / 2) / 3)) * i, 0])(usb()) for i in range(3)])
+
+def top():
+    return difference()(
+        pyramid(),
+        translate([front_size[0] / 2, 20, ])(usbs()))
+
+front = difference()(
+    union()(
+        cube(front_size),
+        translate([wall_thickness, wall_thickness,front_size[2]])(
+            pyramid())),
+    translate([front_size[0] / 2, front_size[1] / 4, 0])(
+        usbs()))
 
 scad_render_to_file(front, 'PBB-Front.scad')
